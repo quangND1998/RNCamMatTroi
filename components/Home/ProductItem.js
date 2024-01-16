acf80bbf01a61b080b84bec26c235d917678b9ed
Nguyen Nga <ngant1096@gmail.com>
2024-01-16 18:04:55 +0700

    edit

--- components/Home/ProductItem.js
+++ components/Home/ProductItem.js
@@ -12,21 +12,34 @@ import {
 import { useHelper } from '../../helpers/helper';
 import React from 'react';
 const { width, height } = Dimensions.get('screen');
+const imageHeight = height - 240;
 import QRCode from 'react-native-qrcode-svg';
 import { Box, Flex } from 'native-base';
 import { ArrowRight } from 'iconsax-react-native';
-const ProductItem = ({ item, navigation }) => {
+const ProductItem = ({ item,index , navigation }) => {
   // console.log(navigation);
   const translateYImage = new Animated.Value(40);
   const { formatDateShort, formatDateUse } = useHelper();
   const handlerDetail = () => {
-    console.log(item.id);
+    console.log(index);
+    
     // console.log(navigation);
-    navigation.navigate('Schedule', {
-      itemId: item.id,
+    // navigation.navigate('Schedule', {
+    //   itemId: item.id,
 
-    });
+    // });
   }
+  const images = [
+    {
+        banner: require('../../assets/images/anhcam.png')
+    },
+    {
+        banner: require('../../assets/images/anhcam2.png')
+    },
+    {
+        banner: require('../../assets/images/anhcam3.png')
+    },
+];
   Animated.timing(translateYImage, {
     toValue: 0,
     duration: 1000,
@@ -34,36 +47,71 @@ const ProductItem = ({ item, navigation }) => {
     easing: Easing.bounce,
   }).start();
   return (
-    <Box className="bg-white rounded-2xl my-2 mx-4  border-solid border-2 border-[#2C5524]">
-      <Box className="flex flex-row p-4 w-full justify-between">
-        <Box style={styles.content} className="text-left mr-2 ">
-          <Text numberOfLines={3} className="text-sm text-gray-700 px-1 my-1 font-base clamp three-lines"
-            style={styles.description}>Mã cây: <Text className="text-[#184E17]">{item.tree?.address}</Text></Text>
-          <Text numberOfLines={3} className="text-sm text-gray-700 px-1 my-1 font-base clamp three-lines"
-            style={styles.description}>Tên cây: <Text className="text-[#FF6100]">{item.tree?.name}</Text></Text>
-          <Text numberOfLines={3} className="text-sm text-gray-700 px-1 my-1 font-base clamp three-lines"
-            style={styles.description}>Ngày nhận nuôi: <Text className="text-[#184E17]">{formatDateShort(item.time_approve)}</Text></Text>
-          <Text numberOfLines={3} className="text-sm text-gray-700 px-1 my-1 font-base clamp three-lines"
-            style={styles.description}>Số ngày nhận nuôi: <Text className="text-[#184E17]">{formatDateUse(item.time_approve)} </Text></Text>
+    <View style={styles.container}>
+      <View style={styles.content} className="relative">
+        <Image
+              className=" w-full object-cover"
+              source={images[index].banner}
+              alt={`imageslide`}
+              style={[
+                styles.image
+              ]}
+            >
+        </Image>
+        <Box className="mt-[-220px] bg-white rounded-2xl  mx-4  border-solid border-2 border-[#2C5524]">
+          <Box className="flex flex-row p-4 w-full justify-between">
+            <Box style={styles.content} className="text-left mr-2 ">
+              <Text numberOfLines={3} className="text-sm text-gray-700 px-1 my-1 font-base clamp three-lines"
+                style={styles.description}>Mã cây: <Text className="text-[#184E17]">{item.tree?.address}</Text></Text>
+              <Text numberOfLines={3} className="text-sm text-gray-700 px-1 my-1 font-base clamp three-lines"
+                style={styles.description}>Tên cây: <Text className="text-[#FF6100]">{item.tree?.name}</Text></Text>
+              <Text numberOfLines={3} className="text-sm text-gray-700 px-1 my-1 font-base clamp three-lines"
+                style={styles.description}>Ngày nhận nuôi: <Text className="text-[#184E17]">{formatDateShort(item.time_approve)}</Text></Text>
+              <Text numberOfLines={3} className="text-sm text-gray-700 px-1 my-1 font-base clamp three-lines"
+                style={styles.description}>Số ngày nhận nuôi: <Text className="text-[#184E17]">{formatDateUse(item.time_approve)} </Text></Text>
+            </Box>
+            <Box className="p-1 ">
+              <QRCode
+                value={`https://qly.cammattroi.com/tree/qrcode/${item?.id}`}
+                logo={require('../../assets/images/product.png')}
+                logoSize={20}
+                logoBackgroundColor='white'
+              />
+            </Box>
+          </Box>
+          <TouchableOpacity onPress={handlerDetail} className="bg-[#FF6100]  flex flex-row rounded-b-2xl items-center justify-center text-white" >
+            <Text numberOfLines={3} className="text-sm text-white uppercase px-1 py-3 font-bold"
+            >Đặt lịch thăm vườn ngay</Text>
+            <ArrowRight className="text-[#ffffff] "
+              size="25"
+              color="#FFFFFF"
+            />
+          </TouchableOpacity>
         </Box>
-        <Box className="p-1 ">
-          <QRCode
-            value={`https://qly.cammattroi.com/tree/qrcode/${item?.id}`}
-            logo={require('../../assets/images/product.png')}
-            logoSize={20}
-            logoBackgroundColor='white'
-          />
+      </View>
+      <Box className="bg-white rounded-2xl">
+        <Box className="mx-4">
+          <Text  className="text-2xl text-[#FF6100]  px-1 py-3 font-bold" >Lịch sử chăm sóc cây</Text>
+          <Box className="" >
+              {item?.tree.history_care ? item.tree?.history_care.map((history, index_h) =>
+                <Box className="flex mx-3">
+                   <Text className="font-base text-sm text-[#184E17] ">{history.date}</Text>
+                   <Box className="flex flex-row my-2 items-center">
+                   <Text className="font-bold text-2xl text-[#184E17] px-3">.</Text>
+                    {history.activity_care ? history.activity_care.map((activity,index_a) => 
+                      <Text className="font-inter font-normal text-base text-[#080808] ">{activity.name} ,</Text>
+                    ) : null}
+                   </Box>
+                </Box>
+              ) : null}
+          </Box>
+          <Text  className="text-2xl text-[#FF6100]  px-1 py-3 font-bold" >Hình ảnh và video</Text>
+
         </Box>
+        
       </Box>
-      <TouchableOpacity onPress={handlerDetail} className="bg-[#FF6100]  flex flex-row rounded-b-2xl items-center justify-center text-white" >
-        <Text numberOfLines={3} className="text-sm text-white uppercase px-1 py-3 font-bold"
-        >Đặt lịch thăm vườn ngay</Text>
-        <ArrowRight className="text-[#ffffff] "
-          size="25"
-          color="#FFFFFF"
-        />
-      </TouchableOpacity>
-    </Box>
+    
+    </View>
   );
 };
 export default ProductItem;
@@ -73,7 +121,7 @@ const styles = StyleSheet.create({
     width: width,
   },
   content: {
-    alignItems: 'left',
+    flex: 1,
   },
   title: {
     fontSize: 24,
@@ -88,5 +136,11 @@ const styles = StyleSheet.create({
   price: {
     fontSize: 32,
     fontWeight: 'bold',
+  },
+  image: {
+    flex: 1,
+    height: imageHeight,
+    width: '100%',
+
   },
 });
--- components/Home/SlideBG.js
+++ components/Home/SlideBG.js
@@ -6,15 +6,19 @@ import {
     Animated,
     Easing,
 } from 'react-native';
-import { Center, Container, Heading, Button, Text, Box, Stack, Input, SearchBar, Icon, Spacer, ZStack, Image, HStack, VStack, Pressable, FlatList, Avatar, useToast } from 'native-base';
+import { Center, Container, Heading, Button, Text, Box, Stack, Input, SearchBar, Spacer, ZStack, Image, HStack, VStack, Pressable, FlatList, Avatar, useToast } from 'native-base';
 import { useDispatch, useSelector } from 'react-redux'
 LogBox.ignoreLogs(["EventEmitter.removeListener"]);
 import Payoo from '../../payoo';
 import { PaymentService } from '../../common/payment/paymentService';
 import { getNews } from '../../store/actions/new';
 import { getActivity } from '../../store/actions/new';
+import { getProductOwner } from '../../store/actions/productService';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import ProductItem from './ProductItem';
 import ChildItem from './ChildItem.js';
+import AppIntroSlider from 'react-native-app-intro-slider';
+import Icon from 'react-native-vector-icons/Ionicons';
 const merchantId = "11931"
 const NewSwiper = ({ navigation, route }) => {
     const envDevelopment = 0;
@@ -28,19 +32,20 @@ const NewSwiper = ({ navigation, route }) => {
     const [refreshing, setRefreshing] = React.useState(false);
     const [index, setIndex] = useState(0);
     const scrollX = useRef(new Animated.Value(0)).current;
-
+    const productOwner = useSelector(state => state.productService.productOwners);
     useEffect(() => {
-        fetchNews();
+        fetchProductOwner();
     }, []);
-    const fetchNews = async () => {
-        dispatch(getNews())
-    }
+    const fetchProductOwner = async () => {
+        dispatch(getProductOwner())
 
+    }
     const onRefresh = React.useCallback(() => {
         setRefreshing(true);
         setTimeout(() => {
-            fetchNews();
+            fetchProductOwner();
             setRefreshing(false);
+            console.log(productOwner);
         }, 2000);
     }, []);
 
@@ -70,28 +75,98 @@ const NewSwiper = ({ navigation, route }) => {
         {
             banner: require('../../assets/images/anhcam3.png')
         },
-    ]
+    ];
+    const slides = [
+        {
+          key: 'one',
+          title: 'Title 1',
+          text: 'Description.\nSay something cool',
+          image: require('../../assets/images/anhcam.png'),
+          backgroundColor: '#59b2ab',
+        },
+        {
+          key: 'two',
+          title: 'Title 2',
+          text: 'Other cool stuff',
+          image: require('../../assets/images/anhcam2.png'),
+          backgroundColor: '#febe29',
+        },
+        {
+          key: 'three',
+          title: 'Rocket guy',
+          text: 'I\'m already out of descriptions\n\nLorem ipsum bla bla bla',
+          image: require('../../assets/images/anhcam3.png'),
+          backgroundColor: '#22bcb5',
+        }
+      ];
     const viewabilityConfig = useRef({
         itemVisiblePercentThreshold: 50,
     }).current;
-    return (
-        <View >
-            {images ?
-                <FlatList
-                    data={images}
-                    renderItem={({ item }) => <ChildItem item={item} />}
-                    horizontal
-                    pagingEnabled
+    _renderItem = ({ item }) => {
+        return (
+          <View style={styles.slide}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Image source={item.image} />
+            <Text style={styles.text}>{item.text}</Text>
+          </View>
+        );
+      }
+      _renderNextButton = () => {
+        return (
+          <View style={styles.buttonCircle}>
+            <Icon
+              name="chevron-forward"
+              color="rgba(255, 255, 255, .9)"
+              size={24}
+            />
+          </View>
+        );
+      };
+      _renderDoneButton = () => {
+        return (
+          <View style={styles.buttonCircle}>
+            <Icon
+              name="md-checkmark"
+              color="rgba(255, 255, 255, .9)"
+              size={24}
+            />
+          </View>
+        );
+      };
+
+        // <View >
+        //     {images ?
+        //         <FlatList
+        //             data={images}
+        //             renderItem={({ item }) => <ChildItem item={item} />}
+        //             horizontal
+        //             pagingEnabled
                     snapToAlignment="center"
-                    showsHorizontalScrollIndicator={false}
-                    onScroll={handleOnScroll}
-                    viewabilityConfig={viewabilityConfig}
-                /> : <View></View>}
-        </View>
-    );
+        //             showsHorizontalScrollIndicator={false}
+        //             onScroll={handleOnScroll}
+        //             viewabilityConfig={viewabilityConfig}
+        //         /> : <View></View>}
+        // </View>
+        if(productOwner){
+            return <View></View>;
+        }else{
+            return  (<AppIntroSlider  data={productOwner} 
+            renderItem={({ item }) => <ProductItem item={item} navigation={navigation} />}
+            renderDoneButton={this._renderDoneButton}
+            renderNextButton={this._renderNextButton}
+            />)
+        }
 }
 
 const styles = StyleSheet.create({
+    buttonCircle: {
+        width: 40,
+        height: 40,
+        backgroundColor: 'rgba(0, 0, 0, .2)',
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
 })
 
 
